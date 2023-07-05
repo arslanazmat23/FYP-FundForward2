a1cc68e116abe2f6236c47179a466a0ff29f41cc
DaniaJavedKhan <djaved034@gmail.com>
2023-07-05 21:49:44 +0500

    Updated

--- components/layout/components/HeaderLogo.js
+++ components/layout/components/HeaderLogo.js
@@ -2,22 +2,22 @@ import styled from 'styled-components';
 
 const HeaderLogo = () => {
   return (
-    <Logo>FundForward</Logo>,
-    <Logo1>waesrdtfyguhij</Logo1>
+    //<Logo>FundForward</Logo>,
+    <Logo1>FundForward</Logo1>
   )
 }
 
-const Logo = styled.h1`
-display: flex;
-  font-weight: normal;
-  font-size: 40px;
-  margin-left: 11px;
-  margin-top: 10px
-  font-family: 'Damion';
-  letter-spacing: 1px;
-  cursor: pointer;
-  color:${(props) => props.theme.labelcolor};
-`
+// const Logo = styled.h1`
+// display: flex;
+//   font-weight: normal;
+//   font-size: 40px;
+//   margin-left: 11px;
+//   margin-top: 10px
+//   font-family: 'Damion';
+//   letter-spacing: 1px;
+//   cursor: pointer;
+//   color:${(props) => props.theme.labelcolor};
+// `
 const Logo1 = styled.h4`
   font-weight: normal;
   font-size: 40px;
